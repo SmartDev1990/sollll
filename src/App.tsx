7baa3e6b8c757116be3d630672c1ba7c227c4b22
nanda yusra <nandayusra100@gmail.com>
2022-01-21 23:39:33 +0700

    gsgsgsgsgsgs

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ import {
 
 import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
 import { ThemeProvider, createTheme } from "@material-ui/core";
-import  cat2 from "media/cat2.jpeg";
 
 const theme = createTheme({
   palette: {
